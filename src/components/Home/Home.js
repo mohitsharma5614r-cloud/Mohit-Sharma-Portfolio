6f1c56faf274a7b58fa7823c9f8b734597ec7599
mohit231802 <aniketsharma23r@gmail.com>
2025-11-13 21:08:39 +0530

    Home: update Find Me On links to GitHub, X, and LinkedIn profiles

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -59,7 +59,7 @@ function Home() {
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/soumyajit4419"
+                  href="https://github.com/mohitsharma5614r-cloud"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -69,7 +69,7 @@ function Home() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://twitter.com/Soumyajit4419"
+                  href="https://x.com/MohitSharma5614"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -79,7 +79,7 @@ function Home() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.linkedin.com/in/soumyajit4419/"
+                  href="https://www.linkedin.com/in/mohit-sharma-981547316/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
