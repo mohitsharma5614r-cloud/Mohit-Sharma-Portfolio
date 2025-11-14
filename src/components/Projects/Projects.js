39469cef25ccab545fa1f2ed3528d1643052bd30
mohit231802 <aniketsharma23r@gmail.com>
2025-11-14 12:24:23 +0530

    Projects: remove Face Recognition and Emotion Detection card

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,7 +3,6 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import circleverseImg from "../../Assets/Projects/circleverse.png";
-import emotion from "../../Assets/Projects/emotion.png";
 import dataScienceImg from "../../Assets/Projects/data science.png";
 import covedex from "../../Assets/Projects/Covedex.png";
 import buildflowImg from "../../Assets/Projects/buildflow.png";
@@ -75,17 +74,7 @@ function Projects() {
             />
           </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
-            />
-          </Col>
+          
         </Row>
       </Container>
     </Container>
